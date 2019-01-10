4bb033b3bc3aa605ac7c013c6de8dbe7a41771a9
GUIDI MARIO <guidi.mario@gmail.com>
2019-01-10 17:28:00 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -4,6 +4,7 @@ sudo apt-get --allow-unauthenticated install -qq -y build-essential git m4 sudo 
 if [ -z ${ISTRAVIS+x} ]; 
 then
 	git clone https://github.com/cmangos/mangos-classic.git
+	git checkout -b last-good-commit d6ab92783a25d6239a2bfcd8d5fd7829ef7e3512
 fi
 git apply MangosPI.patch
 export DEBIAN_FRONTEND=noninteractive
